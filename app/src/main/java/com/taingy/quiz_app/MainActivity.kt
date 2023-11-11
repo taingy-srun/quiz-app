a3089d61fe316c3df843257a06f41071e7d17d14
taingy-srun <taingy.srun20@gmail.com>
2023-11-11 11:40:17 -0800

    first commit

---
+++ app/src/main/java/com/taingy/quiz_app/MainActivity.kt
@@ -0,0 +1,57 @@
+package com.taingy.quiz_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import android.widget.Toast
+import java.text.DateFormat
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.util.Date
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val rg = findViewById<RadioGroup>(R.id.radio_group)
+        val rb1 = findViewById<RadioButton>(R.id.rb_1)
+        val rb2 = findViewById<RadioButton>(R.id.rb_2)
+        val rb3 = findViewById<RadioButton>(R.id.rb_3)
+        val rb4 = findViewById<RadioButton>(R.id.rb_4)
+        val cb1 = findViewById<CheckBox>(R.id.cb_1)
+        val cb2 = findViewById<CheckBox>(R.id.cb_2)
+        val cb3 = findViewById<CheckBox>(R.id.cb_3)
+        val cb4 = findViewById<CheckBox>(R.id.cb_4)
+        val btSubmit = findViewById<Button>(R.id.bt_submit)
+        val btReset = findViewById<Button>(R.id.bt_reset)
+
+        btSubmit.setOnClickListener {
+            var score = 0
+            if (rb3.isChecked) {
+                score += 50
+            }
+            if (cb1.isChecked && cb2.isChecked && cb3.isChecked && !cb4.isChecked) {
+                score += 50
+            }
+
+            val date = LocalDateTime.now()
+            val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd hh:mm")
+            val formattedDate = date.format(formatter)
+            Toast.makeText(this, "Congratulations! You submitted on ${formattedDate}, you achieved ${score}%!", Toast.LENGTH_LONG).show()
+        }
+
+        btReset.setOnClickListener {
+            rg.clearCheck()
+            cb1.isChecked = false
+            cb2.isChecked = false
+            cb3.isChecked = false
+            cb4.isChecked = false
+        }
+
+    }
+}
